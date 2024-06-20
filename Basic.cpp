766119fc3c5d9a6f7656fbfebe559ae743376918
pravi04 <pravi.anand.theraysofthesun@gmail.com>
2024-06-20 11:38:15 +0530

    Added Iterator

--- Basic.cpp
+++ Basic.cpp
@@ -6,17 +6,27 @@ using namespace std;
 int main()
 {
     vector<int> v;
-    int n = v.size();
 
     v.push_back(10);
     v.push_back(20);
     v.push_back(30);
     v.push_back(40);
 
+    int n = v.size();
+
     for (int i = 0; i < n; i++)
     {
 
         cout << v[i] << " ";
+        }
+    cout << endl;
+
+    vector<int>::iterator it = v.begin();
+
+    while (it != v.end())
+    {
+        cout << *it << " ";
+        it++;
     }
 
     return 0;
